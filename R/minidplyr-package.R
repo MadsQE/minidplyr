29e2db4b8adedef56718e36f021135b1b7103b1a
Mads Qvist Ebbesen <au278125@uni.au.dk>
2021-08-26 09:17:16 +0200

    Function 1

---
+++ R/minidplyr-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+usethis::use_description()
+usethis::use_namespace()
+dir.create("R")
+usethis::use_package_doc()
+usethis::use_roxygen_md()
